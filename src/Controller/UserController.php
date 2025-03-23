e43b6f77de2b9952509e4b92c7545b00210ebd79
Matiss Jansons <matiss93jansons@gmail.com>
2025-03-23 16:57:51 +0200

    cleanup: comment out unused routes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,74 +18,74 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route(name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+//    #[IsGranted('ROLE_ADMIN')]
+//    #[Route(name: 'app_user_index', methods: ['GET'])]
+//    public function index(UserRepository $userRepository): Response
+//    {
+//        return $this->render('user/index.html.twig', [
+//            'users' => $userRepository->findAll(),
+//        ]);
+//    }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+//    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+//    public function new(Request $request, EntityManagerInterface $entityManager): Response
+//    {
+//        $user = new User();
+//        $form = $this->createForm(UserType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->render('user/new.html.twig', [
+//            'user' => $user,
+//            'form' => $form,
+//        ]);
+//    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
+//    #[IsGranted('ROLE_ADMIN')]
+//    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+//    public function show(User $user): Response
+//    {
+//        return $this->render('user/show.html.twig', [
+//            'user' => $user,
+//        ]);
+//    }
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
+//    #[IsGranted('ROLE_ADMIN')]
+//    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+//    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+//    {
+//        $form = $this->createForm(UserType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager->flush();
+//
+//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->render('user/edit.html.twig', [
+//            'user' => $user,
+//            'form' => $form,
+//        ]);
+//    }
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
+//    #[IsGranted('ROLE_ADMIN')]
+//    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+//    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+//    {
+//        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+//            $entityManager->remove($user);
+//            $entityManager->flush();
+//        }
+//
+//        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//    }
 
     /**
      * @throws ContainerExceptionInterface
